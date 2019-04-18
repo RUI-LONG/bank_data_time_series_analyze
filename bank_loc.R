9915ff057a8198690dbeca95295f5821d3c216a7
RL <m61216051116@gmail.com>
2019-04-18 22:19:47 +0800

    init push
    Testing ggTimeSeries as visual output

--- bank_loc.R
+++ bank_loc.R
@@ -1,7 +1,14 @@
 library(readxl)
+library(stringr)
+library(shiny)
+library(ggplot2)
+library(data.table)
+
+setwd('E:/bank_data_time_series_analyze')
 data_loc <- read_excel("BANK_LOC_ALL_EL.xlsx")
 data_mct <- read_excel("BANK_MCT_ALL_EL.xlsx")
 
+if(!exists("foo", mode="function")) source("ggplot_waterfall.R")
 
 ## Check data dimension
 #dim(data_mct)
@@ -10,5 +17,27 @@ data_mct <- read_excel("BANK_MCT_ALL_EL.xlsx")
 
 #View(merged_data)
 merged_data = rbind(data_mct, data_loc)
-View(merged_data)
+
+
+
+# 62 2
+merged_data[1:62,3] 
+
+
+
+# creating some data
+set.seed(1)
+dfData = data.frame(x = merged_data[1:62,1] , y = merged_data[1:62,3] )
+
+# base plot
+p1 = ggplot_waterfall(
+  dtData = dfData,
+  'x',
+  'y'
+)
+
+# adding some formatting
+p1 +
+  xlab(NULL) +
+  ylab(NULL)
 
