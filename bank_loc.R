5d50261eba8bba489524fce9355e9cc4537417a3
RL <m61216051116@gmail.com>
2019-04-19 12:27:09 +0800

    04.19 Stage Push

--- bank_loc.R
+++ bank_loc.R
@@ -18,26 +18,29 @@ if(!exists("foo", mode="function")) source("ggplot_waterfall.R")
 #View(merged_data)
 merged_data = rbind(data_mct, data_loc)
 
-
+Taipei <- merged_data[grep("台北市", merged_data$地區), ]
+#View(Taipei)
 
 # 62 2
-merged_data[1:62,3] 
+#merged_data[1:62,3]
 
 
-
+# Visualization 
 # creating some data
 set.seed(1)
-dfData = data.frame(x = merged_data[1:62,1] , y = merged_data[1:62,3] )
+dfData = data.frame(x = 1:61,
+                    y = Taipei[1:61,3], 
+                    inherit.aes = FALSE )
 
 # base plot
 p1 = ggplot_waterfall(
-  dtData = dfData,
-  'x',
-  'y'
+   dtData = Taipei,
+   '地區',
+   '食品餐飲類博士[筆數]'
 )
 
 # adding some formatting
+
 p1 +
   xlab(NULL) +
   ylab(NULL)
-
