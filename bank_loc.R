e666e6d9fec4019604b16733f6340c2f4d0175b1
RL <m61216051116@gmail.com>
2019-04-20 15:39:24 +0800

    04.19 shiny server <build>

--- bank_loc.R
+++ bank_loc.R
@@ -1,46 +1,118 @@
+library(xts)
+library(Rcpp)
+library(shiny)
+library(tidyr)
+library(dplyr)
 library(readxl)
 library(stringr)
-library(shiny)
 library(ggplot2)
+library(TSstudio)
 library(data.table)
 
 setwd('E:/bank_data_time_series_analyze')
 data_loc <- read_excel("BANK_LOC_ALL_EL.xlsx")
 data_mct <- read_excel("BANK_MCT_ALL_EL.xlsx")
 
-if(!exists("foo", mode="function")) source("ggplot_waterfall.R")
+if(!exists("foo", mode="function")) source("E:/bank_data_time_series_analyze/ggTimeSeries-master/R/ggplot_waterfall.R")
+if(!exists("foo", mode="function")) source("E:/bank_data_time_series_analyze/ggTimeSeries-master/R/stat_steamgraph.R")
+### change "107年12月" to "107-12"
 
-## Check data dimension
-#dim(data_mct)
-#dim(data_loc)
-#dim(merged_data)
+AD_convert <- function(date){
+   date <- gsub("月","",gsub("年","/",date))
+   
+   
+   return(date)
+}
 
-#View(merged_data)
+
+
 merged_data = rbind(data_mct, data_loc)
 
 Taipei <- merged_data[grep("台北市", merged_data$地區), ]
-#View(Taipei)
+N_Taipei <- merged_data[grep("新北市", merged_data$地區), ]
+Taoyuan <- merged_data[grep("桃園市", merged_data$地區), ]
+Taichung <- merged_data[grep("台中市", merged_data$地區), ]
+Tainan <- merged_data[grep("台南市", merged_data$地區), ]
+Kaohsiung <- merged_data[grep("高雄市", merged_data$地區), ]
 
-# 62 2
-#merged_data[1:62,3]
+Taipei[,1] <- apply(Taipei[,1],1,FUN=AD_convert)
+N_Taipei[,1] <- apply(N_Taipei[,1],1,FUN=AD_convert)
+Taoyuan[,1] <- apply(Taoyuan[,1],1,FUN=AD_convert)
+Taichung[,1] <- apply(Taichung[,1],1,FUN=AD_convert)
+Tainan[,1] <- apply(Tainan[,1],1,FUN=AD_convert)
+Kaohsiung[,1] <- apply(Kaohsiung[,1],1,FUN=AD_convert)
+View(Taipei)
 
 
-# Visualization 
+# Convert title to null
+names(Taipei) <- NULL
+names(N_Taipei) <- NULL
+names(Taoyuan) <- NULL
+names(Taichung) <- NULL
+names(Tainan) <- NULL
+names(Kaohsiung) <- NULL
+
+# Visualization
 # creating some data
-set.seed(1)
-dfData = data.frame(x = 1:61,
-                    y = Taipei[1:61,3], 
-                    inherit.aes = FALSE )
 
-# base plot
+# convert from exponential notation to normal notation
+options(scipen = 999)
+
+dfData = data.frame(x = 1:61,
+                    y = Taipei[1:61,3])
+
+
+
 p1 = ggplot_waterfall(
-   dtData = Taipei,
-   '地區',
-   '食品餐飲類博士[筆數]'
+   dtData = dfData,
+
+   'x',
+   'y'
 )
 
-# adding some formatting
 
 p1 +
-  xlab(NULL) +
-  ylab(NULL)
+  xlab('時間') +
+  ylab('筆數')
+
+
+# creating some data
+set.seed(10)
+dtData = data.frame(
+   Time = 1:61,
+   Signal =
+      c(
+         as.numeric(as.character(unlist(Taipei[1:61,4]))),
+         as.numeric(as.character(unlist(N_Taipei[1:61,4]))),
+         as.numeric(as.character(unlist(Taoyuan[1:61,4]))),
+         as.numeric(as.character(unlist(Taichung[1:61,4]))),
+         as.numeric(as.character(unlist(Tainan[1:61,4]))),
+         as.numeric(as.character(unlist(Kaohsiung[1:61,4])))
+
+      ),
+
+   VariableLabel = c(rep('臺北市', 61),
+                     rep('新北市', 61), rep('桃園市', 61),
+                     rep('台中市', 61), rep('台南市', 61),
+                     rep('高雄市', 61))
+)
+
+# base plot
+p2 = ggplot(dtData, aes(x = Time, y = Signal,
+                        group = VariableLabel, fill = VariableLabel)) +stat_steamgraph()
+
+
+# Area plot
+#p1 +
+#   xlab('時間') +
+#   ylab('各縣市')
+
+# df = data.frame(x = 1:61,
+#                y = Taipei[,3:4])
+
+
+# ggplot(Taipei, aes(x = 'x' , y = 'y' )) + 
+#    geom_area(aes(color = 'y.1', fill = 'y.2'), 
+#              alpha = 0.5, position = position_dodge(0.8)) +
+#    scale_color_manual(values = c("#00AFBB", "#E7B800")) +
+#    scale_fill_manual(values = c("#00AFBB", "#E7B800"))
